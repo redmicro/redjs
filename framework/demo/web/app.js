1da5ef3b140f7c7f56f3933425a00e6f93586767
dwj_redmicro.cn <107936959@qq.com>
2020-07-11 10:45:50 +0800

    Update app.js

--- framework/demo/web/app.js
+++ framework/demo/web/app.js
@@ -43,7 +43,7 @@ Rsd.createApplication({
 
             var _theme = Rsd.getUrlParam('theme')||'gov';
             var _class = 'Rsd.template.'+_theme+'.web.T' + _tpl + 'Page';
-            console.log(_class);
+            //console.log(_class);
             Rsd.create(_class, { spm:_spm,margin:_m.join(' '),}).show();
         }
     }
