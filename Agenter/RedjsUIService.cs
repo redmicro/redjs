129a78e7d2a4dbb871a73f87df0295610e0876cf
dwj_redmicro.cn <107936959@qq.com>
2020-12-08 12:46:31 +0800

    升级

--- Agenter/RedjsUIService.cs
+++ Agenter/RedjsUIService.cs
@@ -32,7 +32,7 @@ namespace Rsd.Redjs.Agenter
 
             string url = context.Request.RawUrl;  //不含域名和端口
             //redjs 对外主站
-            string _redjsHost = context.Request.Url.Scheme + "://" + context.Request.Url.Host + ":" + context.Request.Url.Port;
+            string _redjsHost = "//" + context.Request.Url.Host + ":" + context.Request.Url.Port;
              
             var _path = new[] { "plugin/" };
             if (_path == null)
